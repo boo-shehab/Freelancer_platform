9987c3229f13939f82078bfe66fdd448e315bf60
Ahmed Abas Al-Ali <bfhetyrhyr12@gmail.com>
2024-12-05 23:23:28 +0300

    add projectPost

---
+++ src/components/ProjectPost/ProjectPost.jsx
@@ -0,0 +1,67 @@
+import CommentsIcon from "../../CustomIcons/CommentsIcon";
+import HeartIcon from "../../CustomIcons/HeartIcon";
+import MoreIcon from "../../CustomIcons/MoreIcon";
+import Card from "../Card/card";
+import CustomButton from "../customButton/CustomButton";
+import styles from "./ProjectPost.module.css"
+const projectPost = ({post}) => {
+    return (
+        <>
+            <Card marginTop={16} key={post.id}>
+                <div className={styles.postItem}>
+                    <div className={styles.postHead}>
+                        <div className={styles.postClient}>
+                            <div className={styles.postAvatar}></div>
+                            <div>
+                                <b className={styles.postClientName}>
+                                    {post?.client?.name}
+                                </b>
+                                <br />
+                                <small className={styles.postClientDate}>
+                                    {post?.client?.createdAt}
+                                </small>
+                            </div>
+                        </div>
+                        <div className={styles.postClientAction}>
+                            <div className={styles.tag}>Available</div>
+                            <MoreIcon />
+                        </div>
+                    </div>
+                    <div className={styles.postBody}>
+                        <b className={styles.postTitle}>{post?.title}</b>
+                        <p className={styles.postDesc}>{post.desc}</p>
+                    </div>
+                    {post?.image ? (
+                        <img className={styles.postImage} src={post?.image} />
+                    ) : (
+                        <div>
+                            <div className={styles.moreInfo}>
+                                <b className={styles.infoTitle}>Duration of project</b>
+                                <p className={styles.infoValue}>{post.duration}</p>
+                            </div>
+                            <div className={styles.moreInfo}>
+                                <b className={styles.infoTitle}>Pricing</b>
+                                <p className={styles.infoValue}>
+                                    Hourly $ {post.price}
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                    <div className={styles.postFooter}>
+                        <div className={styles.footerItem}>
+                            <HeartIcon /> <span>like</span>
+                        </div>
+                        <div className={styles.footerItem}>
+                            <CommentsIcon /> <span>comment</span>
+                        </div>
+                        <CustomButton style={{margin: '0px', marginLeft: 'auto', width: 'auto'}}>
+                            Apply now
+                        </CustomButton>
+                    </div>
+                </div>
+            </Card>
+        </>
+    );
+}
+
+export default projectPost;
--- src/components/customButton/CustomButton.jsx
+++ src/components/customButton/CustomButton.jsx
@@ -1,7 +1,7 @@
 import styles from './CustomButton.module.css'
-const CustomButton = ({onClick, disabled, className = '', children}) => {
+const CustomButton = ({onClick, disabled, className = '', style, children}) => {
     return (
-        <button type="button" onClick={onClick} disabled={disabled} className={`${styles.customButton} ${className}`}>{children}</button>
+        <button type="button" onClick={onClick} disabled={disabled} className={`${styles.customButton} ${className}`} style={style}>{children}</button>
     )
 }
 

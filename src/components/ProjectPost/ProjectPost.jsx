49fc4b4fcb7d867fe6511adfd38da08791d7ed6b
maryamadel22 <maryamadel22>
2024-12-07 21:26:58 +0300

    add list post

--- src/components/ProjectPost/ProjectPost.jsx
+++ src/components/ProjectPost/ProjectPost.jsx
@@ -3,65 +3,79 @@ import HeartIcon from "../../CustomIcons/HeartIcon";
 import MoreIcon from "../../CustomIcons/MoreIcon";
 import Card from "../Card/card";
 import CustomButton from "../customButton/CustomButton";
-import styles from "./ProjectPost.module.css"
-const projectPost = ({post}) => {
+import styles from "./ProjectPost.module.css";
+import { useState } from "react";
+const projectPost = ({ post }) => {
+  const [isListVisible, setVisiblePostId] = useState(null);
+  const idShow = (id) => {
+    setVisiblePostId((prevId) => (prevId === id ? null : id));
+  };
     return (
-        <>
-            <Card marginTop={16} key={post.id}>
-                <div className={styles.postItem}>
-                    <div className={styles.postHead}>
-                        <div className={styles.postClient}>
-                            <div className={styles.postAvatar}></div>
-                            <div>
-                                <b className={styles.postClientName}>
-                                    {post?.client?.name}
-                                </b>
-                                <br />
-                                <small className={styles.postClientDate}>
-                                    {post?.client?.createdAt}
-                                </small>
-                            </div>
-                        </div>
-                        <div className={styles.postClientAction}>
-                            <div className={styles.tag}>Available</div>
-                            <MoreIcon />
-                        </div>
-                    </div>
-                    <div className={styles.postBody}>
-                        <b className={styles.postTitle}>{post?.title}</b>
-                        <p className={styles.postDesc}>{post.desc}</p>
-                    </div>
-                    {post?.image ? (
-                        <img className={styles.postImage} src={post?.image} />
-                    ) : (
-                        <div>
-                            <div className={styles.moreInfo}>
-                                <b className={styles.infoTitle}>Duration of project</b>
-                                <p className={styles.infoValue}>{post.duration}</p>
-                            </div>
-                            <div className={styles.moreInfo}>
-                                <b className={styles.infoTitle}>Pricing</b>
-                                <p className={styles.infoValue}>
-                                    Hourly $ {post.price}
-                                </p>
-                            </div>
-                        </div>
-                    )}
-                    <div className={styles.postFooter}>
-                        <div className={styles.footerItem}>
-                            <HeartIcon /> <span>like</span>
-                        </div>
-                        <div className={styles.footerItem}>
-                            <CommentsIcon /> <span>comment</span>
-                        </div>
-                        <CustomButton style={{margin: '0px', marginLeft: 'auto', width: 'auto'}}>
-                            Apply now
-                        </CustomButton>
-                    </div>
+    <>
+      <Card marginTop={16} key={post.id}>
+        <div className={styles.postItem}>
+          <div className={styles.postHead}>
+            <div className={styles.postClient}>
+              <div className={styles.postAvatar}></div>
+              <div>
+                <b className={styles.postClientName}>{post?.client?.name}</b>
+                <br />
+                <small className={styles.postClientDate}>
+                  {post?.client?.createdAt}
+                </small>
+              </div>
+            </div>
+            <div className={styles.postClientAction}>
+              <div className={styles.tag}>Available</div>
+              <MoreIcon
+                onClick={() => idShow(post.id)}
+
+              />
+
+              {isListVisible===post.id && (
+                <div className={styles.list} style={{marginTop: "220px", position: "absolute" }}>
+                <button>Share</button>
+                  <button>Copy link</button>
+                 <button>Report</button>
                 </div>
-            </Card>
-        </>
-    );
-}
+              )}
+            </div>
+          </div>
+          <div className={styles.postBody}>
+            <b className={styles.postTitle}>{post?.title}</b>
+            <p className={styles.postDesc}>{post.desc}</p>
+          </div>
+          {post?.image ? (
+            <img className={styles.postImage} src={post?.image} />
+          ) : (
+            <div>
+              <div className={styles.moreInfo}>
+                <b className={styles.infoTitle}>Duration of project</b>
+                <p className={styles.infoValue}>{post.duration}</p>
+              </div>
+              <div className={styles.moreInfo}>
+                <b className={styles.infoTitle}>Pricing</b>
+                <p className={styles.infoValue}>Hourly $ {post.price}</p>
+              </div>
+            </div>
+          )}
+          <div className={styles.postFooter}>
+            <div className={styles.footerItem}>
+              <HeartIcon /> <span>like</span>
+            </div>
+            <div className={styles.footerItem}>
+              <CommentsIcon /> <span>comment</span>
+            </div>
+            <CustomButton
+              style={{ margin: "0px", marginLeft: "auto", width: "auto" }}
+            >
+              Apply now
+            </CustomButton>
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+};
 
 export default projectPost;
